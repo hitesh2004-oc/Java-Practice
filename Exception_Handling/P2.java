fe8f10f8b9e752d93b3af6a35229a90146568fec
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-07-07 08:44:23 +0530

    Exception Handling Practice

---
+++ Exception_Handling/P2.java
@@ -0,0 +1,13 @@
+package Exception_Handling;
+
+public class P2 {
+	void Display() {
+		System.out.println("Hello");
+	}
+	public static void main(String[] args) {
+		P2 p = null;
+     	p.Display();
+	
+	}
+
+}
