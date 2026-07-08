19c606f8dade85d0332c372ff2367262d3e71b6a
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-07-08 08:53:43 +0530

    Implement main method with arithmetic exception
    Added main method with divide by zero example

--- Exception_Handling/P1.java
+++ Exception_Handling/P1.java
@@ -1,5 +1,12 @@
 package Exception_Handling;
 
 public class P1 {
-
+  public static void main(String arg[]) throws{
+    int a = 10;
+    int b = 0;
+    // Divide by Zero
+    // Arithmetic Exception
+    int c = a/b;
+    System.out.print(c);
+  }
 }
