017a82452bfbac444952eaec92eed5819155250d
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-07-23 07:19:11 +0530

    Practice

---
+++ C.java
@@ -0,0 +1,20 @@
+package com.malviya;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class C {
+	public static void main(String[] args) {
+	HashMap<Integer,String> hm = new HashMap<>();
+	hm.put(1, "A");
+	hm.put(2, "B");
+	hm.put(3, "C");
+	
+//	Iterator<Integer> i = (Iterator<Integer>) hm.entrySet();
+//	while(i.hasNext()) {
+//		Integer s = i.next();
+//		String a = hm.get(s);
+//		System.out.println(s +": "+a);
+//	  }
+	}
+}
