436d8ff21ce964882a9b0cb1b3bed4980210e0ac
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-07-30 07:18:34 +0530

    Practice

---
+++ Lambda Expression Practice/P2.java
@@ -0,0 +1,18 @@
+package PracticeOfLambdaExp;
+
+//...........[Easy Question]...........//
+//(2)Check if Number is Even/Odd//
+
+interface I2{
+	void checkWeatherOfNumber(int num);
+	
+}
+public class P2 {
+	public static void main(String[] args) {
+		I2 i = (num) -> {if(num%2 == 0) {
+			System.out.println("Even");
+			}else System.out.println("Odd");
+		};
+		i.checkWeatherOfNumber(10);
+	}
+}
