240a20ea753851cf3767801dc3b17d0225f6a348
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-08-14 08:57:09 +0530

    Enhance P1.java with comments for clarity
    Added comments to clarify file creation process.

--- P1.java
+++ P1.java
@@ -4,11 +4,12 @@ import java.io.File;
 
 public class P1 {
 	public static void main (String[] args) {
+		// Using this File Class We Create File
 	  File f = new File("Data");
 	  boolean s1 = f.mkdir();
 	  
 	 File a = new File(f , "test.txt");
-	 
+	 // it will give us Exception So we use Try & Catch
 	 try {
 		 a.createNewFile();
 	 }
